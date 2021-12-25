53be3f34d97724e22657b2cadbf9e6e44f3d7552
kennedybabu <kennedynjogu8@gmail.com>
2021-12-25 11:17:07 +0300

    feature: login a user to the application

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
     path('', views.home, name='home'),
     path('hood/<int:pk>/', views.hood, name='hood'), 
     path('create_hood/', views.createHood, name='create_hood'),
--- app/views.py
+++ app/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Hood, Location
 from .forms import HoodForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 
 
 # Create your views here.
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try: 
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'user doesnt exist')
+
+        user = authenticate(request, username= username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password does not exist')
+
+    context = {
+
+    }
+    return render(request, 'app/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
