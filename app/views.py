bd58ac589300e5ed300d2c6f5a1d88bdf142ed6c
kennedybabu <kennedynjogu8@gmail.com>
2022-01-04 13:15:21 +0300

    style: style the create business template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Hood, Location, Post, User
+from .models import Hood, Location, Post, User, Business
 from .forms import HoodForm, UserForm, MyUserCreationForm, BusinessForm
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
